e142d63b1f0ef35cf27bcf65361eb0022a58e32b
Antony Jose <antony4shopping@gmail.com>
2019-10-13 23:23:54 +0530

    'baseurl'

--- src/axios-orders.js
+++ src/axios-orders.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL : 'https://react-my-burger.firebaseio.com/'
+    baseURL : 'https://my-react-burger-builder-58fbd.firebaseio.com/'
 });
 
 export default instance;
